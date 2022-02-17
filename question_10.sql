6328eaa1ad11f22d2b8eba83c616cd0c4f78d7ab
Tim <cocatimothy96@gmail.com>
2022-02-17 11:57:35 -0500

    okay now it is done

--- question_10.sql
+++ question_10.sql
@@ -18,6 +18,20 @@ SELECT *
 FROM max_hr_table
 WHERE hr = most_hrs
 AND yearid = 2016
+AND hr <> 0;
+
+WITH yil_table AS (SELECT DISTINCT playerid, MAX(yearid) OVER(PARTITION BY playerid) - MIN(yearid) OVER(PARTITION BY playerid) AS years_in_league
+				   FROM appearances
+				   ORDER BY playerid),
+  max_hr_table AS (SELECT playerid, yearid, hr, MAX(hr) OVER(PARTITION BY playerid) AS most_hrs
+				   FROM batting)
+SELECT DISTINCT CONCAT(namefirst, ' ', namelast) AS player_name, most_hrs AS HRS_2016
+FROM people AS p 
+JOIN yil_table AS yil
+ON p.playerid = yil.playerid
+JOIN max_hr_table AS mht
+ON p.playerid = mht.playerid
+WHERE yil.years_in_league >= 10
+AND yearid = 2016
 AND hr <> 0
-
-
+ORDER BY player_name;
